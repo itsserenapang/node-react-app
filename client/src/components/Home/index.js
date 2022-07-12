9b486c0746c3b010c6fc272e65d32e7f06175494
Serena Pang <serena.hy.pang@gmail.com>
2022-07-12 11:39:27 -0400

    project 2!!

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,18 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
+import TextField from "@material-ui/core/TextField";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Radio from "@material-ui/core/Radio";
+import Button from "@material-ui/core/Button";
+import FormHelperText from "@material-ui/core/FormHelperText";
 import Paper from "@material-ui/core/Paper";
+import { FormLabel } from '@material-ui/core';
 
 
 //Dev mode
-const serverURL = ""; //enable for dev mode
+//const serverURL = ""; //enable for dev mode
 
 //Deployment mode instructions
-//const serverURL = "http://ov-research-4.uwaterloo.ca:PORT"; //enable for deployed mode; Change PORT to the port number given to you;
+const serverURL = "http://ec2-18-188-101-79.us-east-2.compute.amazonaws.com::3035"; //enable for deployed mode; Change PORT to the port number given to you;
+// const serverURL = ""; 
 //To find your port number: 
 //ssh to ov-research-4.uwaterloo.ca and run the following command: 
 //env | grep "PORT"
@@ -20,153 +31,289 @@ const serverURL = ""; //enable for dev mode
 
 const fetch = require("node-fetch");
 
-const opacityValue = 0.9;
-
-const theme = createTheme({
-  palette: {
-    type: 'dark',
-    background: {
-      default: "#000000"
-    },
-    primary: {
-      main: "#52f1ff",
-    },
-    secondary: {
-      main: "#b552f7",
-    },
-  },
-});
-
-const styles = theme => ({
-  root: {
-    body: {
-      backgroundColor: "#000000",
-      opacity: opacityValue,
-      overflow: "hidden",
-    },
-  },
-  mainMessage: {
-    opacity: opacityValue,
-  },
-
-  mainMessageContainer: {
-    marginTop: "20vh",
-    marginLeft: theme.spacing(20),
-    [theme.breakpoints.down('xs')]: {
-      marginLeft: theme.spacing(4),
-    },
-  },
-  paper: {
-    overflow: "hidden",
-  },
-  message: {
-    opacity: opacityValue,
-    maxWidth: 250,
-    paddingBottom: theme.spacing(2),
-  },
-
-});
-
-
-class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userID: 1,
-      mode: 0
-    }
-  };
-
-  componentDidMount() {
-    //this.loadUserSettings();
-  }
-
-
-  loadUserSettings() {
-    this.callApiLoadUserSettings()
-      .then(res => {
-        //console.log("loadUserSettings returned: ", res)
-        var parsed = JSON.parse(res.express);
-        console.log("loadUserSettings parsed: ", parsed[0].mode)
-        this.setState({ mode: parsed[0].mode });
-      });
-  }
-
-  callApiLoadUserSettings = async () => {
-    const url = serverURL + "/api/loadUserSettings";
-
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        //authorization: `Bearer ${this.state.token}`
-      },
-      body: JSON.stringify({
-        userID: this.state.userID
+export default function Home() {
+  return (
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Typography variant="h3" component="h2">
+        Review a Movie
+      </Typography>
+      <Review>
+      </Review>
+    </Grid>
+  )
+}
+const Review =({}) => {
+  const [movies, setMovies] = React.useState([])
+  React.useEffect(() => {
+    async function loadMovies() {
+      const url = serverURL + "/api/getMovies"
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        }
       })
-    });
-    const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    console.log("User settings: ", body);
-    return body;
+      if (response.status !== 200) throw Error(body.message);
+      const movieList = await response.json()
+      setMovies(movieList)
+      console.log(movieList)
+    }
+    loadMovies()
+  }, [])
+  const [select, setSelect] = React.useState("")
+  function updateSelect(event){
+    setSelect(event.target.value)
   }
+  const [title, setTitle] = React.useState("")
+  function updateTitle(event){
+    setTitle(event.target.value)
+    console.log(title)
+  }
+  const [body, setBody] = React.useState("")
+  function updateBody(event){
+    setBody(event.target.value)
+    console.log(body)
+  }
+  const [rating, setRating] = React.useState("")
+  function updateRating(event){
+    setRating(event.target.value)
+    console.log(rating)
+  }
+  const [userID, setuserID] = React.useState(1)
+  const [validSelect, setValidSelect] = React.useState(true)
+  const [validTitle, setValidTitle] = React.useState(true)
+  const [validBody, setValidBody] = React.useState(true)
+  const [validRating, setValidRating] = React.useState(true)
+  const [valid, setValid] = React.useState(true)
+  const [storedValues, setStoredValues] = React.useState([])
+  const [showCon, setShowCon] = React.useState(false)
+  function checkValid(){
+    if (select !== "") {
+      setValidSelect(true)
+    } else {
+      setValidSelect(false)
+      setValid(false)
+      return false
+    }
+    if (title !== "") {
+      setValidTitle(true)
+    } else {
+      setValidTitle(false)
+      setValid(false)
+      return false
+    }
+    if (body !== "") {
+      setValidBody(true)
+    } else {
+      setValidBody(false)
+      setValid(false)
+      return false
+    }
+    if (rating !== "") {
+      setValidRating(true)
+    } else {
+      setValidRating(false)
+      setValid(false)
+      return false
+    }
+    setValid(true)
+    return true
+  }
+  function handleSubmit(){
+    if (checkValid()) {
+        const review = {
+          select,
+          title,
+          body,
+          rating,
+          userID
+        }
 
-  render() {
-    const { classes } = this.props;
+        async function addReview() {
+          const url = serverURL + "/api/addReview"
+          const response = await fetch(url, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(review)
+    
+          })
+          if (response.status !== 200) throw Error(body.message);
+        }
 
+        addReview()
 
+        setShowCon(true)
+        setTimeout(() =>{
+          setShowCon(false)
+        }, 1000)
 
-    const mainMessage = (
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        justify="flex-start"
-        alignItems="flex-start"
-        style={{ minHeight: '100vh' }}
-        className={classes.mainMessageContainer}
+        setStoredValues([review, ...storedValues])
+        setSelect("")
+        setTitle("")
+        setBody("")
+        setRating("")
+    }
+  }
+  return(
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <MovieSelection
+        select = {select}
+        handleChange = {updateSelect}
+        validSelect = {validSelect}
+        movies = {movies}
       >
-        <Grid item>
+      </MovieSelection>
+      <ReviewTitle
+        title = {title}
+        handleChange = {updateTitle}
+        validTitle = {validTitle}
+        >
+      </ReviewTitle>
+      <ReviewBody
+        body = {body}
+        handleChange = {updateBody}
+        validBody = {validBody}
+      >
+      </ReviewBody>
+      <ReviewRating
+        rating = {rating}
+        handleChange = {updateRating}
+        validRating = {validRating}
+      >
+      </ReviewRating>
+      <Submit
+        handleSubmit = {handleSubmit}
+        valid = {valid}
+      >
+      </Submit>
+      <br/>
+      {
+        showCon 
+        &&
+        <div>Successfully saved review</div>  
+      }
+      <DisplayReviews 
+        reviews = {storedValues}
+        />
+    </Grid>
 
-          <Typography
-            variant={"h3"}
-            className={classes.mainMessage}
-            align="flex-start"
-          >
-            {this.state.mode === 0 ? (
-              <React.Fragment>
-                Welcome to MSci245!
-              </React.Fragment>
-            ) : (
-              <React.Fragment>
-                Welcome back!
-              </React.Fragment>
-            )}
-          </Typography>
-
-        </Grid>
-      </Grid>
-    )
-
-
-    return (
-      <MuiThemeProvider theme={theme}>
-        <div className={classes.root}>
-          <CssBaseline />
-          <Paper
-            className={classes.paper}
-          >
-            {mainMessage}
-          </Paper>
-
-        </div>
-      </MuiThemeProvider>
-    );
-  }
+  )
+}
+const MovieSelection = ({select, handleChange, validSelect, movies}) => {
+  return (
+    <FormControl variant = "outlined">
+      <FormLabel id="demo-simple-select-outlined-label">Movie Selection</FormLabel>
+      <Select 
+        labelId="label" 
+        id="select" 
+        value={select} 
+        onChange={handleChange} 
+        error={!validSelect}
+      >
+        {
+          movies.map((movie) => {
+            return (
+              <MenuItem value={movie.id}>{movie.name}</MenuItem>
+            )
+          })
+        }
+      </Select>
+    </FormControl>
+  )
+}
+const ReviewTitle = ({title, handleChange, validTitle}) => {
+  return (
+    <FormControl variant = "outlined">
+        <FormLabel id="demo-simple-select-outlined-label">Review Title</FormLabel>
+        <TextField 
+          id="standard-basic"
+          error={!validTitle}
+          value={title} 
+          onChange={handleChange}
+          helperText={!validTitle && "Please enter your review title"}
+        />
+    </FormControl>
+  )
 }
 
-Home.propTypes = {
-  classes: PropTypes.object.isRequired
-};
+const ReviewBody = ({body, handleChange, validBody}) => {
+  return (
+    <FormControl variant = "outlined">
+      <FormLabel id="demo-simple-select-outlined-label">Review</FormLabel>
+      <TextField
+            id="filled-multiline-flexible"
+            multiline
+            inputProps={{ maxLength: 200 }}
+            rows={10}
+            variant="filled"
+            value={body}
+            onChange={handleChange}
+            error={!validBody}
+            helperText={!validBody && "Please enter your review"}
+        />
+    </FormControl>
+  )
+}
+const ReviewRating = ({rating, handleChange, validRating}) => {
+  return (
+    <FormControl variant = "outlined" >
+      <FormLabel id="demo-simple-select-outlined-label" >Rating</FormLabel>
+      <RadioGroup 
+        aria-label="gender" 
+        name="gender1" 
+        value={rating} 
+        onChange={handleChange} 
+        error={!validRating}
+        >
+        <FormControlLabel value="1" control={<Radio />} label="1" />
+        <FormControlLabel value="2" control={<Radio />} label="2" />
+        <FormControlLabel value="3" control={<Radio />} label="3" />
+        <FormControlLabel value="4" control={<Radio />} label="4" />
+        <FormControlLabel value="5" control={<Radio />} label="5" />
+        <FormHelperText >{!validRating && "Please enter your review rating"}</FormHelperText>
+      </RadioGroup>
+    </FormControl>
+  )
+}
+const Submit = ({handleSubmit, valid}) => {
+  return (
+    <Button variant="contained" onClick={handleSubmit} >Submit</Button>
+  )
+}
+const DisplayReviews = ({reviews}) => {
+  return (
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+    {reviews.map(review=>{
+      return(<DisplayReview review = {review}/>)
+    })}
+    </Grid>
+  )
+}
+const DisplayReview = ({review}) => {
+  return (
+    <div>
+    <div> Movie Title: {review.select}</div>
+    <div> Review Title: {review.title}</div>
+    <div>Review: {review.body}</div>
+    <div>Rating: {review.rating}</div>
+    </div>
+  )
+}
 
-export default withStyles(styles)(Home);
--- server.js
+++ server.js
@@ -36,7 +36,60 @@ app.post('/api/loadUserSettings', (req, res) => {
 	connection.end();
 });
 
+app.post('/api/getMovies', (req, res) => {
+	console.log('get movies')
+	let connection = mysql.createConnection(config);
+	let userID = req.body.userID;
+
+	let sql = `SELECT * from movies`;
+	console.log(sql);
+	let data = [userID];
+	console.log(data);
+
+	connection.query(sql, data, (error, results, fields) => {
+		if (error) {
+			return console.error(error.message);
+		}
+
+		// let string = JSON.stringify(results);
+		//let obj = JSON.parse(string);
+		res.send(results);
+	});
+	connection.end();
+});
 
 
-app.listen(port, () => console.log(`Listening on port ${port}`)); //for the dev version
-//app.listen(port, '129.97.25.211'); //for the deployed version, specify the IP address of the server
+app.post('/api/addReview', (req, res) => {
+	console.log('add review')
+	console.log(req.body)
+	let connection = mysql.createConnection(config);
+	// let userID = req.body.userID;
+	const title = req.body.title;
+	const body = req.body.body;
+	const rating = req.body.rating;
+	const userID = req.body.userID;
+	const select = req.body.select;
+	
+	let sql = `INSERT INTO Review (reviewTitle, reviewContent, reviewScore, userID, movieID) VALUES ('${title}', '${body}', '${rating}', '${userID}', '${select}')`;
+	console.log(sql);
+	// let data = [userID];
+	// console.log(data);
+	//ASK CHLOE AND ESHA ------------------------
+	//-------------------------------------------
+	connection.query(sql, (error, results) => {
+		if (error) {
+			return console.error(error.message);
+		}
+		console.log(results)
+
+		// let string = JSON.stringify(results);
+		//let obj = JSON.parse(string);
+		res.send(results);
+	});
+	connection.end();
+});
+
+
+
+// app.listen(port, () => console.log(`Listening on port ${port}`)); //for the dev version
+app.listen(port, '172.31.31.77'); //for the deployed version, specify the IP address of the server
