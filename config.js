e2f0a133abaf52d858c26b42aea43bbf147867c4
Serena Pang <serena.hy.pang@gmail.com>
2022-07-26 00:17:20 -0400

    fix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 let config = {
-  host    : 'ec2-18-216-101-119.us-east-2.compute.amazonaws.com',
+  host    : 'ec2-18-188-101-79.us-east-2.compute.amazonaws.com',
   user    : 's32pang',
   password: 'password',
   database: 's32pang'
